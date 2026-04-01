a6814fc7e569c49806aaa31655f248c3e565f0e2
timcrepinpro <tim.crepin@edouardgand.fr>
2026-04-01 14:43:04 +0200

    premier test auto

--- TpTestsCalculatrice.App/Program.cs
+++ TpTestsCalculatrice.App/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace TpTestsCalculatrice.App;
+﻿
+
+namespace TpTestsCalculatrice.App;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Calculatrice calc = new Calculatrice(); 
+ 
+        Console.WriteLine(calc.Addition(2, 3)); 
+        Console.WriteLine(calc.Division(10, 2)); 
     }
 }
+
+
+/*
+car il teste pour tres peut de possibiliter
+
+
+*/
---
+++ TpTestsCalculatrice.Tests/CalculatriceTests.cs
@@ -0,0 +1,15 @@
+﻿namespace TpTestsCalculatrice.Tests; 
+ 
+[TestClass] 
+public class CalculatriceTests 
+{ 
+    [TestMethod] 
+    public void Addition_Retourne5_Si2Plus3() 
+    { 
+        Calculatrice calc = new Calculatrice(); 
+ 
+        int resultat = calc.Addition(2, 3); 
+ 
+        Assert.AreEqual(5, resultat); 
+    } 
+} 
